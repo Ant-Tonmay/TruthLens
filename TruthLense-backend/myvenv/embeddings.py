b314d2c972c02387d0c876f5abea4f7274cc0ab9
Tonmay Sardar <tonmaysardar500@gmail.com>
2025-04-29 01:16:33 +0530

    Local Model Added :all-MiniLM-L6-v2

---
+++ TruthLense-backend/myvenv/embeddings.py
@@ -0,0 +1,14 @@
+from langchain.embeddings import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer
+
+model_name = "all-MiniLM-L6-v2"
+model_path = model_name
+
+model = SentenceTransformer(model_path)
+def get_embedding(text):
+    return model.encode(text).tolist()
+
+def get_embedding_model():
+    return HuggingFaceEmbeddings(
+        model_name=model_path 
+    )
